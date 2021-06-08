3ed45a8ac85181adf78319c5961a61e017768bcc
igor-silva <igor-d-silva@hotmail.com>
2021-06-08 17:47:57 -0300

    primeiro commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,64 @@
+﻿import React, {useEffect, useState} from 'react';
+import {
+    View, 
+    Text, 
+    Image, 
+    StyleSheet
+} from 'react-native';
+import userImg from '../assets/pp.png';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+export function Header(){
+    const [userName, setUserName] = useState<string>();
+
+    useEffect(() => {
+        async function loadStorageUserName() {
+            const user =await AsyncStorage.getItem('@plantmanger:user');
+            setUserName(user || '');
+        }
+        loadStorageUserName()
+
+    },[]);
+
+    return(
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>Olá,</Text>
+                <Text  style={styles.userName}>
+                    {userName}
+                </Text>
+            </View>
+            <Image source={userImg} style={styles.image}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight()
+    },
+    greeting:{
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName:{
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        lineHeight: 40
+    },
+    image:{
+        width: 70,
+        height: 70,
+        borderRadius: 40
+    }
+});
---
+++ src/components/PlantCardPrimary.tsx
@@ -0,0 +1,60 @@
+﻿import React, { useEffect, useState } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    FlatList
+} from 'react-native';
+import { RectButton, RectButtonProps} from 'react-native-gesture-handler';
+import { SvgFromUri } from 'react-native-svg';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+interface PlantProps extends RectButtonProps{
+    data: {
+        name: string;
+        photo: string;
+    }
+}
+
+export const PlantCardPrimary = ({ data, ...rest} : PlantProps) => {
+    return(
+        <RectButton
+            style={styles.container}
+            {...rest}
+        >
+            <SvgFromUri 
+                uri={ data.photo } 
+                width={ 70 } 
+                height={ 70 } 
+            />
+            <Text style={styles.text}>
+                { data.name }
+            </Text>
+        </RectButton>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        maxWidth: '45%',
+        backgroundColor: colors.shape,
+        borderRadius: 20,
+        paddingVertical: 10,
+        alignItems: 'center',
+        margin: 10
+
+    },
+    header:{
+        paddingHorizontal: 30
+    },
+    text:{
+        fontSize: 17,
+        color: colors.green_dark,
+        fontFamily: fonts.heading,
+        lineHeight: 20,
+        marginVertical: 16
+    }
+});
